3a7ad4c134333f4a8b6421cffa5e8219b0ffe5c8
SABITRON <thesh@VenelisHusktop>
2025-03-05 01:30:47 -0600

    Added a track for Ocean Biomes, courtesy of HeartsRemedy

--- src/main/java/net/sabitron/alternia/init/AlterniaModSounds.java
+++ src/main/java/net/sabitron/alternia/init/AlterniaModSounds.java
@@ -17,4 +17,5 @@ public class AlterniaModSounds {
 	public static final DeferredRegister<SoundEvent> REGISTRY = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, AlterniaMod.MODID);
 	public static final RegistryObject<SoundEvent> MUS_ALTERNIA_1 = REGISTRY.register("mus.alternia_1", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation("alternia", "mus.alternia_1")));
 	public static final RegistryObject<SoundEvent> MUS_GRIEFED_RESPITEBLOCK = REGISTRY.register("mus.griefed_respiteblock", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation("alternia", "mus.griefed_respiteblock")));
+	public static final RegistryObject<SoundEvent> MUS_ALTERNIA_OCEAN = REGISTRY.register("mus.alternia_ocean", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation("alternia", "mus.alternia_ocean")));
 }
